22fed1094b2908c97f4412ee4f9ba9352f5b6e19
N0tACyb0rg <caleb.xavier.marshall@gmail.com>
2024-03-27 11:46:17 -0700

    Commenting is partially done.

--- src/main/java/ravenrobotics/shootloops/commands/autos/ppcommands/AutoShootCommand.java
+++ src/main/java/ravenrobotics/shootloops/commands/autos/ppcommands/AutoShootCommand.java
@@ -7,34 +7,44 @@ import ravenrobotics.shootloops.subsystems.IntakeSubsystem;
 
 public class AutoShootCommand extends Command
 {
+    //Declare the Intake and Flywheel subsystems.
     private final IntakeSubsystem intakeSubsystem;
     private final FlywheelSubsystem flywheelSubsystem;
 
     private boolean isDone = false;
 
+    /**
+     * Command for shooting a note into the speaker during autonomous.
+     */
     public AutoShootCommand()
     {
+        //Get the instances for the subsystems.
         this.intakeSubsystem = IntakeSubsystem.getInstance();
         this.flywheelSubsystem = FlywheelSubsystem.getInstance();
-
+        //Add the subsystems as requirements so none of them are being used while the command is running.
         addRequirements(intakeSubsystem, flywheelSubsystem);
     }
 
     @Override
     public void initialize()
     {
+        //Turn on the flywheel for shooting.
         flywheelSubsystem.shootOn();
     }
 
     @Override
     public void execute()
     {
+        //Wait for both flywheel arrays to spin up to 8000 RPM.
         while (flywheelSubsystem.getBottomVelocity() < 8000 && flywheelSubsystem.getTopVelocity() < 8000)
         {
             continue;
         }
+        //Wait 0.1 seconds.
         Timer.delay(0.1);
+        //Turn on the intake to shoot out the note.
         intakeSubsystem.runRollersFlywheel();
+        //Wait 1.5 seconds to be done.
         Timer.delay(1.5);
         isDone = true;
     }
@@ -42,6 +52,7 @@ public class AutoShootCommand extends Command
     @Override
     public void end(boolean isInterrupted)
     {
+        //Stop the flywheels and the rollers.
         flywheelSubsystem.stopFly();
         intakeSubsystem.stopRollers();
     }
--- src/main/java/ravenrobotics/shootloops/commands/autos/ppcommands/IntakeNoteCommand.java
+++ src/main/java/ravenrobotics/shootloops/commands/autos/ppcommands/IntakeNoteCommand.java
@@ -4,39 +4,47 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import ravenrobotics.shootloops.subsystems.DriveSubsystem;
 import ravenrobotics.shootloops.subsystems.IntakeSubsystem;
-import ravenrobotics.shootloops.subsystems.IntakeSubsystem.IntakeArmPosition;
 
 public class IntakeNoteCommand extends Command 
 {
+    //Declare the drive and intake subsystems.
     private final DriveSubsystem driveSubsystem;
     private final IntakeSubsystem intakeSubsystem;
 
+    /**
+     * Command for intaking a note during autonomous. CONFIGURE WITH A TIMEOUT.
+     */
     public IntakeNoteCommand()
     {
+        //Get the instances of the subsystems.
         this.driveSubsystem = DriveSubsystem.getInstance();
         this.intakeSubsystem = IntakeSubsystem.getInstance();
-
+        //Add the subsystems as requirements.
         addRequirements(driveSubsystem, intakeSubsystem);
     }
 
     @Override
     public void execute()
     {
+        //Turn on the intake rollers.
         intakeSubsystem.runRollersIntake();
+        //Drive at a slow-ish speed that is fine for just picking up a note.
         driveSubsystem.drive(new ChassisSpeeds(0.125, 0, 0));
     }
 
     @Override
     public void end(boolean isInterrupted)
     {
+        //Stop driving.
         driveSubsystem.drive(new ChassisSpeeds(0, 0, 0));
+        //Stop the rollers.
         intakeSubsystem.stopRollers();
-        intakeSubsystem.setIntakePosition(IntakeArmPosition.kRetracted);
     }
 
     @Override
     public boolean isFinished()
     {
+        //Check if it's finished by checking the note sensor (there's a 3 second timeout for if we don't pick up a note).
         return intakeSubsystem.getDistanceSensor();
     }
 }
